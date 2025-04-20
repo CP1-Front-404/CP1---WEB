88415683c9e98aff7104503c985ad278679ae67c
Nicolas <nico12571@hotmail.com>
2025-04-19 21:38:17 -0300

    Resolvendo o Exercício 2

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,16 @@ var valor2 = null; //Declara uma variavel com valor vazio/nulo
 console.log(valor2);
 var valor3 = 3; // Declara uma variavel com valor
 console.log(valor3);
+
+// Exercício 2
+// ==, != e >= são operadores de comparação que vão comparar se dois valores são ,respectivamente, iguais, diferentes ou se um valor é maior ou igual a outro. 
+var a = 1;
+var b = 1;
+var c = 2;
+console.log(a==b)
+// Se o valor de "a" é IGUAL ao de "b" no console aparecerá true e se não for igual false.
+console.log(a!=b) 
+// Se o valor de "a" é DIFERENTE ao de "b" no console aparecerá true e se não for false.
+console.log(b>=a)
+console.log(c>=a)
+// Se o valor de "a" é maior OU igual ao de "b" no console aparecerá true e se não for false.
