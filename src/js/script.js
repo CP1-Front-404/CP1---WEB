486a29905e3792a6a76ce6e9161e0405c40560dc
Nicolas <nico12571@hotmail.com>
2025-04-19 21:34:58 -0300

    Resolvendo o Exercício 1

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// Exercício 1 
+
+var valor1; // Declara uma variavel com valor undefined
+console.log(valor1);
+var valor2 = null; //Declara uma variavel com valor vazio/nulo
+console.log(valor2);
+var valor3 = 3; // Declara uma variavel com valor
+console.log(valor3);
